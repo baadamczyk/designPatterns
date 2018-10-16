3fc8b36b9dff2e00bd60c7091d934afc6e014972
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2018-10-16 10:05:20 +0200

    Added test class without tests

---
+++ src/test/java/pl/baadamczyk/designpatterns/behavioral/observer/LawnWateringSystemTest.java
@@ -0,0 +1,20 @@
+package pl.baadamczyk.designpatterns.behavioral.observer;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class LawnWateringSystemTest {
+
+    @Test
+    @Disabled("Not implemented")
+    public void shouldTurnOnWateringSystem_whenCommandOnDriverIsExecuted() {
+
+    }
+
+    @Test
+    @Disabled("Not implemented")
+    public void shouldTurnOffWateringSystem_whenCommandOnDriverIsExecuted() {
+
+    }
+}
