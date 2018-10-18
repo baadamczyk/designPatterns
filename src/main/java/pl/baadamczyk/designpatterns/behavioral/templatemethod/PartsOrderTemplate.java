ce2b5b2ec2d198123d8b9a393f1994da488bba8b
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2018-10-18 14:34:37 +0200

    Implemented red tests

---
+++ src/main/java/pl/baadamczyk/designpatterns/behavioral/templatemethod/PartsOrderTemplate.java
@@ -0,0 +1,8 @@
+package pl.baadamczyk.designpatterns.behavioral.templatemethod;
+
+public abstract class PartsOrderTemplate {
+
+    public abstract double doGetItems();
+    public abstract double
+
+}
